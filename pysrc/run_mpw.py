84abc38fab671ee0274a6c78f03539ac8f6441f3
Diptodip Deb <dr.diptodip@gmail.com>
2016-07-14 10:51:31 -0500

    Begin k-disjoint path implementation.

--- pysrc/run_mpw.py
+++ pysrc/run_mpw.py
@@ -1,7 +1,7 @@
 from mpw import *
 from graphs import *
 
-G = binary_digraph(100)
+G = binary_digraph(500)
 while not connected(G, 0):
-    G = binary_digraph(100)
+    G = binary_digraph(500)
 write_mpb(G, 0, "test.mpb")
